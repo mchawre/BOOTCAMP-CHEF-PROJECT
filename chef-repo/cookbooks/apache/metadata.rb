3efdcc1ae5e1f4506aef1266c8e00c14bf623a86
yogesh9391 <yogeshvj.jil;hawar@gmail.com>
2015-08-17 18:35:58 +0530

    modified

---
+++ chef-repo/cookbooks/apache/metadata.rb
@@ -0,0 +1,9 @@
+name 'apache'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '0.4.0'
+
+depends 'apache2', '= 3.1.0'
