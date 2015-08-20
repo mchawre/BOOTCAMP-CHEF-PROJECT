5365743c1b852a667d9adb781860aea93cafc91b
yogesh <yogeshvj.jilhawar@gmail.com>
2015-08-20 10:04:41 +0530

    deleted

--- chef-repo/cookbooks/docker001/README.md
+++
@@ -1,4 +0,0 @@
-# docker001
-
-TODO: Enter the cookbook description here.
-
--- metadata.rb
+++
@@ -1,9 +0,0 @@
-name 'docker001'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures docker001'
-long_description 'Installs/Configures docker001'
-version '0.57.0'
-
-depends 'docker', '= 0.43.0'
